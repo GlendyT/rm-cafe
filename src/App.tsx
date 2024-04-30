c68b9a4f967611c2dc54ed94c0346e1c50160a86
GlendyT <glendytuyuc@gmail.com>
2024-04-29 21:42:37 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <header className=" bg-gray-400 py-2">
+      <h1 className="text-center text-4xl uppercase font-bold">Joonnie´s Cafe</h1>
+    </header>
+    </>
+  )
+}
+
+export default App
